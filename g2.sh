f341f70595eb05229a164ad7e1d26a60a6726f73
Grendel Yang <stevenygd@gmail.com>
2016-09-01 21:40:27 -0400

    [add] Use assume-yes to prevent the trouble of entering Y.

--- g2.sh
+++ g2.sh
@@ -1,27 +1,27 @@
 #! /bin/bash
 
-sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install -y linux-image-extra-`uname -r`
+sudo apt-get update && sudo apt-get -y upgrade --assume-yes
+sudo apt-get install -y linux-image-extra-`uname -r` --assume-yes
 
-sudo apt-get install git
-sudo apt-get install build-essential
-sudo apt-get install cmake
+sudo apt-get install git --assume-yes
+sudo apt-get install build-essential --assume-yes
+sudo apt-get install cmake --assume-yes
 
 # Install CUDA
-sudo apt-get install nvidia-cuda-toolkit
+sudo apt-get install nvidia-cuda-toolkit --assume-yes
 
-wget http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda-repo-ubuntu1404-7-5-local_7.5-18_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1404-7-5-local_7.5-18_amd64.deb
-sudo apt-get update
-sudo apt-get install cuda
+wget http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda-repo-ubuntu1404-7-5-local_7.5-18_amd64.deb --assume-yes
+sudo dpkg -i cuda-repo-ubuntu1404-7-5-local_7.5-18_amd64.deb --assume-yes
+sudo apt-get update --assume-yes
+sudo apt-get install cuda --assume-yes
 
 # Install OPEN CV
-sudo apt-get install libopencv-dev python-opencv
+sudo apt-get install libopencv-dev python-opencv --assume-yes
 
 # install xGhost for display
-sudo apt-get install x11-xserver-utils
-sudo apt-get install xauth
-sudo apt-get install xorg
+sudo apt-get install x11-xserver-utils --assume-yes
+sudo apt-get install xauth --assume-yes
+sudo apt-get install xorg --assume-yes
 
 # Convert/mogrify tools 
-sudo apt-get install imagemagick
+sudo apt-get install imagemagick --assume-ye
