9296ff4d8075bba97e9ff57af267a8ddfe6146e0
Grendel Yang <stevenygd@gmail.com>
2016-10-23 10:17:54 -0400

    [update] Add assume ye Add assume yess

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
 # Open CV stack
-sudo apt-get install build-essential
-sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get install build-essential --assume-yes
+sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev --assume-yes
+sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev --assume-yes
 
 # Scipy and numpy stack
-sudo apt-get install python-pip
-sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
-sudo apt-get install python-skimage
-sudo apt-get install libblas-dev liblapack-dev
+sudo apt-get install python-pip --assume-yes
+sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose --assume-yes
+sudo apt-get install python-skimage --assume-yes
+sudo apt-get install libblas-dev liblapack-dev --assume-yes
 
 # Update to get scipy 1.8+
-pip install --upgrade pip
-sudo apt-get install --upgrade scipy
-sudo apt-get update
+pip install --upgrade pip --assume-yes
+sudo apt-get install --upgrade scipy --assume-yes
+sudo apt-get update --assume-yes
